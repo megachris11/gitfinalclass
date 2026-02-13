b430fe86da6097e0fa7485c7d506cb89c141dba5
Chris Agbonkhese <agbonschris@gmail.com>
2026-02-13 08:34:49 -0500

    The initial commit

---
+++ example.py
@@ -0,0 +1,32 @@
+def sumInFile(filename: str) -> int:
+    infile = open(filename, "r")
+    count = 0
+    line = infile.readline()  # read first line
+    while line != "":
+        count = count + int(line)
+        line = infile.readline()
+    infile.close()
+    return count
+
+def sumOfInts(n: int) -> int:
+    count = 0
+    for i in range(1, n+1, 1):
+        count = count + i
+    return count
+
+def main() -> None:
+    print(f"sumOfInts(9)  = {sumOfInts(9)} (expected: 45)")
+    print(f"sumOfInts(10) = {sumOfInts(10)} (expected: 55)")
+
+    # NOTE: if you try to run this in VS Code by pressing
+    # the play button, your terminal's working directory must
+    # be the same as where data.txt resides; otherwise, you will
+    # get the following error:
+    # FileNotFoundError: [Errno 2] No such file or directory: 'data.txt'
+    filename = "data.txt"
+    print(f"sum of ints in {repr(filename)} = {sumInFile(filename)} (expected: 186)")
+
+#main()  # without the if below, this gets executed on import
+
+if __name__ == "__main__": 
+    main()  # call main only if we are running this program directly
