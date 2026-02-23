e1eb4357985e586aa3f7567df2ba982ab980d20e
megachris <elasticlala@gmail.com>
2026-02-23 07:01:08 -0500

    final commit

--- maincode.py
+++ maincode.py
@@ -1,5 +1,5 @@
 def sumInFile(filename: str) -> int:
-    print("Chris did this..Okay Okay Okay")
+    print("Chris did this..")
     infile = open(filename, "r")
     count = 0
     line = infile.readline()  # read first line
